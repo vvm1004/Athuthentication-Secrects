3bb049fee94459e90bd7b90ae63f985220038eab
vvm1004 <96302642+vvm1004@users.noreply.github.com>
2023-08-23 20:01:31 +0700

    Salting and Hasing passwords

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require('md5');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 app.use(express.static("public"));
@@ -34,29 +34,34 @@ app.get("/register", (req, res) => {
 })
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password),
-    })
-
-    newUser.save()
-        .then(() => {
-            res.render('secrets')
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash,
         })
-        .catch(err =>{
-            console.log(err);
-        }) 
+    
+        newUser.save()
+            .then(() => {
+                res.render('secrets')
+            })
+            .catch(err =>{
+                console.log(err);
+            }) 
+    });
+    
 })
 
 app.post("/login", (req, res) => {
    const username = req.body.username;
-   const password = md5(req.body.password);
+   const password = req.body.password;
 
     User.findOne({ email: username})
         .then((foundUser) => {
-            if(foundUser.password === password){
-                res.render('secrets')
-            }
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if(result == true){
+                    res.render('secrets')
+                }
+            });
         })  
         .catch((err) => {
             console.log(err);
